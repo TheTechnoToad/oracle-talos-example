853f1ea111fa458eb83195f0c5d085e978850bf4
Brian Kanya <briankanya@gmail.com>
2025-02-03 11:16:15 -0500

    Update variables.tf

--- modules/talos/variables.tf
+++ modules/talos/variables.tf
@@ -21,7 +21,7 @@ variable "controlplane_node_ips" {
 }
 
 variable "kubernetes_version" {
-  default     = "1.29.8"
+  default     = "1.32.1"
   description = "The version of Kubernetes to use for the cluster."
   type        = string
 
@@ -34,7 +34,7 @@ variable "kubernetes_version" {
 variable "talos_version" {
   description = "The version of Talos to use for the cluster."
   type        = string
-  default     = "v1.8.0"
+  default     = "v1.9.3"
 
   validation {
     condition     = can(regex("^v[0-9]+\\.[0-9]+\\.[0-9]+$", var.talos_version))
